a90bf61b1d07634819be9fc7f7849299f565098f
rvgorlov <rv.gorlov@gmail.com>
2017-03-31 16:44:55 +0700

    temp commit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import socket
 import wgAPI
 import telebot
 import config
 import threading
+import time
 
 bot = telebot.TeleBot(config.telegramToken)
-isPolling = False
+isPolling = 'no'
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -26,14 +28,84 @@ def polling():
     global isPolling
     while 1:
         try:
-            isPolling = True
+            print('Стартуем бота')
+            isPolling = 'ok'
             bot.polling(none_stop=False, interval=1)
         except:
-            print("Exception")
+            print("Exception (polling)")
 
+def internetListener():
+    global isPolling
+    soc = socket.socket()
+    soc.bind(config.servers[config.current])
+    soc.listen(len(config.servers))
 
+    try:
+        while 1:
+            try:
+                conn, addr = soc.accept()
+                conData = str.encode(isPolling)
+                conn.send(conData)
+            except socket.error as e:
+                print(e)
+            except:
+                print('except')
+            finally:
+                conn.close()
+    finally:
+        soc.close()
 
-pollingThread = threading.Thread(target=polling)
+def serversChecker():
+    soc = socket.socket()
+    soc.settimeout(5)
+    try:
+        while 1:
+            serversStatus = []
+
+            for key in config.servers.keys():
+                if key == config.current:
+                    continue
+                else:
+                    try:
+                        conn, addr = soc.connect( config.servers[key] )
+                        data = conn.recv()
+                        data = data.decode()
+                        serversStatus.append(data)
+                    except socket.error as e:
+                        print(e)
+                    except:
+                        pass
+            isworkServers = False
+
+            for x in serversStatus:
+                print(x)
+                if x == 'ok':
+                    print('Найден работающий бот')
+                    workServers = True
+
+            if not isworkServers:
+                print('Запущенные боты не обнаружены')
+                polling()
+            else:
+                print("Обнаружен работающий бот. Ожидаем его смерти.")
+
+            time.sleep(20)
+
+    except:
+        print('except')
+
+#pollingThread = threading.Thread(target=polling)
+listenThread = threading.Thread(target=internetListener)
+reciverThread = threading.Thread(target=serversChecker)
 
 if __name__ == '__main__':
-    pollingThread.run()
+    try:
+        #pollingThread.run()
+        listenThread.start()
+        reciverThread.start()
+
+        listenThread.join()
+        reciverThread.join()
+        pass
+    except:
+        print("Exception (threads)")
